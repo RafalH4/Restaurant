ee232f1974d4561e1d6520e4c533ca7e8942238f
Rafał Huk <r.huk@outlook.com>
2020-04-08 18:55:41 +0200

    WebAPI: stworzenie modeli: Dish, Order, User

---
+++ RestaurantWebApi/DishDirectory/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebApi.DishDirectory
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string MyProperty { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ RestaurantWebApi/UserDirectory/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebApi.UserDirectory
+{
+    public class User
+    {
+
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
