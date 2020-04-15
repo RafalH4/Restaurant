460ee0a7323d0112c2fdfe99a756ebed92a82392
Rafał Huk <r.huk@outlook.com>
2020-04-15 17:16:43 +0200

    WebApi userRepository complete

---
+++ RestaurantWebApi/UserDirectory/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebApi.UserDirectory
+{
+    public interface IUserRepository
+    {
+        Task Add(User user);
+        Task Delete(User user);
+        Task Update(User user);
+        Task<User> GetByUserName(string email);
+        Task<User> GetById(Guid id);
+
+    }
+}
---
+++ RestaurantWebApi/UserDirectory/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using RestaurantWebApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantWebApi.UserDirectory
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly RestaurantContext _dataContext;
+        public UserRepository(RestaurantContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task Add(User user)
+        {
+            await _dataContext.Users.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task Delete(User user)
+        {
+            _dataContext.Users.Remove(user);
+            await _dataContext.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task<User> GetById(Guid id)
+            => await Task.FromResult(_dataContext.Users.FirstOrDefault(user => user.Id == id));
+
+        public async Task<User> GetByUserName(string email)
+            => await Task.FromResult(_dataContext.Users.FirstOrDefault(user => user.Email == email));
+
+        public async Task Update(User user)
+        {
+            _dataContext.Users.Update(user);
+            await _dataContext.SaveChangesAsync();
+            await Task.CompletedTask;
+
+        }
+    }
+}
